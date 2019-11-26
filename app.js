f9e9b4977f5115865d71857c4b7e59ca9492faa7
chopper011 <dexterorozco13@gmail.com>
2019-11-26 12:47:18 +0100

    temporary remove parse and post

--- app.js
+++ app.js
@@ -1,22 +1,22 @@
 var express = require('express');
 var app = express();
 
-var bodyParser = require('body-parser');
-var urlendcoderParser = bodyParser.urlencoded();
-app.use(bodyParser.urlencoded({ extended: true }));
+// var bodyParser = require('body-parser');
+// var urlendcoderParser = bodyParser.urlencoded();
+// app.use(bodyParser.urlencoded({ extended: true }));
 
 app.get('/', function(req, res){
     res.sendfile(__dirname + '/public/html/index.html');
 })
 
-app.post('/processpost', urlendcoderParser, function(req, res){
-   var data = {
-       firstname: req.body.firstname,
-       lastname: req.body.lastname
-   }
-   console.log(data);
-   res.end(JSON.stringify(data)) 
-})
+// app.post('/processpost', urlendcoderParser, function(req, res){
+//    var data = {
+//        firstname: req.body.firstname,
+//        lastname: req.body.lastname
+//    }
+//    console.log(data);
+//    res.end(JSON.stringify(data)) 
+// })
 
 var server = app.listen(process.env.PORT, function(){
     console.log('Server is up and running ' + server.address().port);
