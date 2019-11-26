c58ca544b09d672a5fe7fdf42d1285f2307e5865
Linus Gyllenberg <linus@sbgdata.se>
2019-11-26 15:07:57 +0100

    Css, input boxes and image added

--- app.js
+++ app.js
@@ -6,18 +6,26 @@ var urlendcoderParser = bodyParser.urlencoded();
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
+app.use(express.static("Public"));
+
 app.get('/', function(req, res){
-    res.sendfile(__dirname + '/public/html/index.html');
+    res.sendFile(__dirname + '/public/html/index.html');
 })
 
-app.post('/processpost', urlendcoderParser, function(req, res){
+app.post('/note', urlendcoderParser, function(req, res){
    var data = {
-       firstname: req.body.firstname,
-       lastname: req.body.lastname
+       Rubrik: req.body.Rubrik,
+       Text: req.body.Text
    }
    console.log(data);
-   res.end(JSON.stringify(data)) 
+   var dataString = JSON.stringify(data);
+   res.end(JSON.stringify(data));
 }) 
+app.get('/note.html', function(req,res){
+    res.sendFile(__dirname + '/public/html/note.html');
+    dataString;
+
+})
 
 var server = app.listen(7757, function(){
     console.log('Server is up and running ' + server.address().port);
