1b2286634dd659d9bfe9df3c092bbfac4ed7dd7c
chopper011 <dexterorozco13@gmail.com>
2019-11-26 12:13:12 +0100

    changed to PORT

--- app.js
+++ app.js
@@ -1,10 +1,23 @@
 var express = require('express');
 var app = express();
 
-app.get('/', function(req, res){
-    res.sendfile(__dirname + 'public/html/index.html')
-});
+var bodyParser = require('body-parser');
+var urlendcoderParser = bodyParser.urlencoded();
+app.use(bodyParser.urlencoded({ extended: true }));
 
-var server = app.listen(4004, function(){
+app.get('/', function(req, res){
+    res.sendfile(__dirname + '/public/html/index.html');
+})
+
+app.post('/processpost', urlendcoderParser, function(req, res){
+   var data = {
+       firstname: req.body.firstname,
+       lastname: req.body.lastname
+   }
+   console.log(data);
+   res.end(JSON.stringify(data)) 
+})
+
+var server = app.listen(process.env.PORT, function(){
     console.log('Server is up and running ' + server.address().port);
-});
+})
