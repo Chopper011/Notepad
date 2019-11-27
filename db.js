e8ea0ea6628fd0ab2ea5a507a14de882fb98dbc0
Linnea <linnea.miderkvist@edu.huddinge.se>
2019-11-27 16:30:27 +0100

    Connection to test db put up for testing

--- db.js
+++ db.js
@@ -2,6 +2,18 @@
 
 //connection
 
+const MongoClient = require('mongodb').MongoClient;
+const uri = "mongodb+srv://LinneaMid:abQZg3BjOq1KqG8B@test-si7mo.azure.mongodb.net/test?retryWrites=true&w=majority";
+const client = new MongoClient(uri, { useNewUrlParser: true });
+client.connect(err => {
+  const collection = client.db("sample_airbnb").collection("listingsAndReviews");
+  // perform actions on the collection object
+
+  
+  client.close();
+});
+
+
 //läs in i db
 
 //läs av collection och skriv ut note på sidan
