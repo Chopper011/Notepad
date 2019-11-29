9ad76b5342fb6f6607eba08ce5f47939beaedd6f
Linnea <linnea.miderkvist@edu.huddinge.se>
2019-11-29 10:28:55 +0100

    Testing without db code in

--- db.js
+++ db.js
@@ -40,13 +40,6 @@ function myFunction() {
 
     document.getElmentById("note").innerHTML = "Notes"; //fix or take out?
    
-    app.get("/note", (req, res) => {
-        collection.find({}).toArray((error, result) => {
-            if(error) {
-                return res.status(500).send(error);
-            }
-            res.send(result);
-        });
-    });
+  
 
   }
